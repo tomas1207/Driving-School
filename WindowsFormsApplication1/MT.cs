f674e54fd9da6dca0a5b076a1103ddc7d52d737c
Tomas1207 <tomasfernandes98@gmail.com>
2016-01-20 10:20:56 +0000

    Desin Update

---
+++ WindowsFormsApplication1/MT.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class MT : Form
+    {
+        string Nome;
+
+        public MT()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+          
+        }
+
+        private void MT_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = BusinessLogicLayer.BLL.Carros.Load();
+            dataGridView2.DataSource = BusinessLogicLayer.BLL.Registo.Load();
+
+        }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BusinessLogicLayer.BLL.Registo.deletregisto(Nome);
+        }
+
+        private void dataGridView2_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Nome = dataGridView2.Rows[e.RowIndex].Cells[0].Value.ToString();
+        }
+    }
+}
---
+++ WindowsFormsApplication1/Rcarro.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Rcarro : Form
+    {
+        public Rcarro()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //lista de carros
+            Lcarro fm4 = new Lcarro();
+            fm4.Show();
+            this.Hide();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            //registo de carros
+
+            int ret = BusinessLogicLayer.BLL.Carros.insertCarros(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, textBox5.Text);
+
+        }
+
+        private void Rcarro_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
